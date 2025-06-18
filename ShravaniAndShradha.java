92ab2bf92faba24310e657bad6968a672eeb9562
shravanii21 <shravanidesai217@gmail.com>
2025-06-18 14:11:31 +0530

    Add files via upload

---
+++ ShravaniAndShradha.java
@@ -0,0 +1,15 @@
+public class ShravaniAndShradha {
+    public void greet(){
+        System.out.println("Hello, this is Shravani's first method speaking.");
+    }
+
+    public void greeting(){
+        System.out.println("Hi, this is Shradha's second method saying hello.");
+    }
+
+    public static void main(String[] args){
+        ShravaniAndShradha obj1 = new ShravaniAndShradha();
+        obj1.greet();
+        obj1.greeting();
+    }
+}
